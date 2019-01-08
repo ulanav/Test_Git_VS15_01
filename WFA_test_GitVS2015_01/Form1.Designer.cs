9663500acb5f4637e423ef6a396240e61924f6c1
ulanav <ulanav@rambler.ru>
2019-01-08 13:27:46 +0300

    restore

--- WFA_test_GitVS2015_01/Form1.Designer.cs
+++ WFA_test_GitVS2015_01/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(319, 283);
+            this.ClientSize = new System.Drawing.Size(174, 78);
             this.Name = "Form1";
             this.Text = "FormTestGit01";
             this.ResumeLayout(false);
