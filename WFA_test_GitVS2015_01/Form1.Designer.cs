211408393a4cb07cd01b3f7b7abaad9ebaa38643
ulanav <ulanav@rambler.ru>
2019-01-08 15:30:42 +0300

    next width form change

--- WFA_test_GitVS2015_01/Form1.Designer.cs
+++ WFA_test_GitVS2015_01/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(466, 292);
+            this.ClientSize = new System.Drawing.Size(483, 292);
             this.Name = "Form1";
             this.Text = "FormTestGit01G";
             this.ResumeLayout(false);
