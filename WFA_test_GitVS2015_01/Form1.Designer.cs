1fbaf9b2a2b08ecd617d0695fc5dcee44cd17c3e
ulanav <ulanav@rambler.ru>
2019-01-08 15:25:34 +0300

    Change form title

--- WFA_test_GitVS2015_01/Form1.Designer.cs
+++ WFA_test_GitVS2015_01/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(409, 294);
             this.Name = "Form1";
-            this.Text = "FormTestGit01";
+            this.Text = "FormTestGit01G";
             this.ResumeLayout(false);
 
         }
